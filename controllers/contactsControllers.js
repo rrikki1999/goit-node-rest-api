48496ef5c342ae1756ce9a7e7ee0f120515aafeb
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-03-25 18:11:37 +0200

    hw 2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,8 +1,31 @@
-import contactsService from "../services/contactsServices.js";
+import { listContacts, getContactById, removeContact, addContact } from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    try {
+        const result = await listContacts();
+        res.json(result);
+    } catch (error) {
+        console.error("Error getting contacts:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await getContactById(id);
+       
+        if (!result){
+            res.status(404).json({ 
+                message: `A contact with such id=${id} was not found`
+            });
+        }
+        res.json(result);
+    } catch (error) {
+        console.error("Error getting contacts:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
 
 export const deleteContact = (req, res) => {};
 
